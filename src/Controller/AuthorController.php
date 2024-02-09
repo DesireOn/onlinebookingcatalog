2e1fa9eff99bbfb315098ba8b27137fd7e6e29f5
Petar Stoyanov <petar.stoyanov@flatrocktech.com>
2024-02-09 14:52:20 +0200

    Change path for authors prefix.

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/author')]
+#[Route('/authors')]
 class AuthorController extends AbstractController
 {
     #[Route('/', name: 'app_author_index', methods: ['GET'])]
