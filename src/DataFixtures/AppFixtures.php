5a3718a0dcfd14fbb2df6196186aad0e55599aff
Petar Stoyanov <petar.stoyanov@flatrocktech.com>
2024-02-09 14:21:46 +0200

    Add fixtures for genre entity.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Factory\AuthorFactory;
 use App\Factory\BookFactory;
+use App\Factory\GenreFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,8 +13,10 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         AuthorFactory::createMany(25);
+        GenreFactory::createMany(25);
         BookFactory::createMany(50, [
-            'author' => AuthorFactory::random()
+            'author' => AuthorFactory::random(),
+            'genre' => GenreFactory::random()
         ]);
         $manager->flush();
     }
