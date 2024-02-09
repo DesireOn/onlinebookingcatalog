00520a3adb970991d85780dbea20492e7562d531
Petar Stoyanov <petar.stoyanov@flatrocktech.com>
2024-02-09 14:24:56 +0200

    Load authors and genres on homepage.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\AuthorRepository;
 use App\Repository\BookRepository;
+use App\Repository\GenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +12,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_book_index', methods: ['GET'])]
-    public function index(BookRepository $bookRepository): Response
+    public function index(
+        BookRepository $bookRepository,
+        AuthorRepository $authorRepository,
+        GenreRepository $genreRepository
+    ): Response
     {
         return $this->render('main/index.html.twig', [
             'books' => $bookRepository->findBy([], ['title' => 'ASC'], 4),
+            'authors' => $authorRepository->findBy([], ['name' => 'ASC'], 3),
+            'genres' => $genreRepository->findBy([], ['title' => 'ASC'], 4),
         ]);
     }
 }
